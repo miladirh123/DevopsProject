325e13db5036e5c7a5ab5038df9caea7d0c6d5ad
Rahma Miladi <ton.email@example.com>
2025-10-08 20:13:27 +0100

    Add SonarQube

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,28 +1,13 @@
 provider "aws" {
-  region     = "us-east-1" # Assure-toi que cette région correspond à ton compte AWS
+  region     = "us-east-1" 
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
-variable "aws_access_key" {
-  type        = string
-  description = "Clé d'accès AWS"
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "Clé secrète AWS"
-}
-
-variable "private_key" {
-  type        = string
-  description = "Clé privée SSH pour provisioner EC2"
-}
-
 resource "aws_instance" "devapp" {
-  ami           = "ami-052064a798f08f0d3" # ← AMI valide que tu as fourni
-  instance_type = "t3.micro"              # ← Compatible avec Free Tier
-  key_name      = "ec2-key"               # ← Nom de ta paire de clés créée sur AWS
+  ami           = "ami-052064a798f08f0d3"
+  instance_type = "t2.micro"
+  key_name      = "ec2-key"
 
   tags = {
     Name = "DevAppInstance"
