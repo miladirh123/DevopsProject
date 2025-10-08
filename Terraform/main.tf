71b56d8523837d088e63396d30dd21247a80a017
Rahma Miladi <ton.email@example.com>
2025-10-08 19:16:52 +0100

    Add SonarQube

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,8 +1,3 @@
-variable "private_key" {
-  type        = string
-  description = "Clé privée SSH pour se connecter à EC2"
-}
-
 resource "aws_instance" "devapp" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
@@ -27,8 +22,4 @@ resource "aws_instance" "devapp" {
       host        = self.public_ip
     }
   }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.devapp.public_ip
 }
--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "ec2_public_ip" {
-  value = aws_instance.jenkins_server.public_ip
+  value = aws_instance.devapp.public_ip
 }
