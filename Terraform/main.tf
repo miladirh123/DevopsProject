176463fd6118e5dc832e55cdac90c4cf318d5701
Rahma Miladi <ton.email@example.com>
2025-10-12 14:13:33 +0100

    Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,37 +1,7 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-resource "aws_security_group" "ssh_access" {
-  name        = "ssh-access"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    description = "SSH from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "SSHAccess"
-  }
-}
-
 resource "aws_instance" "devapp" {
   ami           = "ami-04c08fd8aa14af291"
   instance_type = "t3.micro"
-  key_name      = "ec2-key"
+  key_name      = "ec2-key"  # Nom exact de la clé AWS
   security_groups = [aws_security_group.ssh_access.name]
 
   root_block_device {
