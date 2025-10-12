f2aca37e1af574b49e60a11630726e6b6838c71c
Rahma Miladi <ton.email@example.com>
2025-10-12 13:42:15 +0100

    Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,10 +4,40 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh-access"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SSHAccess"
+  }
+}
+
 resource "aws_instance" "devapp" {
-  ami           = "ami-052064a798f08f0d3"
+  ami           = "ami-04c08fd8aa14af291"
   instance_type = "t3.micro"
   key_name      = "ec2-key"
+  security_groups = [aws_security_group.ssh_access.name]
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp2"
+  }
 
   tags = {
     Name = "DevAppInstance"
@@ -26,6 +56,7 @@ resource "aws_instance" "devapp" {
       user        = "ec2-user"
       private_key = var.private_key
       host        = self.public_ip
+      timeout     = "5m"
     }
   }
 }
