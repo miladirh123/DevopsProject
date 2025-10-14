34d6e7b233754c1ba7f612e3af6b9625c4a63558
Rahma Miladi <ton.email@example.com>
2025-10-14 20:44:10 +0100

    Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,8 +1,23 @@
-resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
- tags = {
-   Name = var.name_tag,
- }
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+resource "aws_instance" "public_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = "ec2-key" # Assure-toi que cette clé existe dans AWS
+  tags = {
+    Name = var.name_tag
+  }
 }
