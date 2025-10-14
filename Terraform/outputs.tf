b89b79bfba9b2663099bed29a490750913f9ca19
Rahma Miladi <ton.email@example.com>
2025-10-14 19:13:54 +0100

    Terraform

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "ec2_public_ip" {
-  value = aws_instance.devapp.public_ip
+  description = "Adresse IP publique de l'instance EC2"
+  value       = aws_instance.devapp.public_ip
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,11 +1,14 @@
 variable "aws_access_key" {
-  type = string
+  type        = string
+  description = "Clé d'accès AWS"
 }
 
 variable "aws_secret_key" {
-  type = string
+  type        = string
+  description = "Clé secrète AWS"
 }
 
 variable "private_key" {
-  type = string
+  type        = string
+  description = "Clé privée SSH pour provisioner"
 }
